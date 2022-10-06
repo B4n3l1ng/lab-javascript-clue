5982ba4be3166f27d6369be736537620e72adef9
Eric Coimbra <eric.coimbra@gmail.com>
2022-10-06 14:50:01 +0100

    Iteration3 Complete

--- src/clue.js
+++ src/clue.js
@@ -140,9 +140,7 @@ function pickMystery() {
 
 
 // ITERATION 3
-
 function revealMystery(envelope) {  
     const string = `${envelope.suspect.firstName} ${envelope.suspect.lastName} killed Mr. Boddy using the ${envelope.weapon.name} in the ${envelope.room.name}!`;
     return string;
 }
-
